ae1c5ddac8d0314be35679b311ff17d9b247acc6
Yohei Toriyama <kagemomiji@gmail.com>
2020-10-31 20:01:51 +0900

    divide crawler get data test

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -28,7 +28,7 @@ class TestCrawler(unittest.TestCase):
 
         env.recover(old_env)
     
-    def test_get_data(self):
+    def test_get_data_10(self):
 
         #Input Test Data
         today = datetime.date.today()
@@ -42,6 +42,32 @@ class TestCrawler(unittest.TestCase):
 
         # assert 
         self.assertEqual(len(crawler.get_data(today.year,today.month)) , 10)
+        
+        #clear test data
+        #get data
+        json = api.get_data()
+
+        #delete payment
+        for data in json:
+            api.delete_payment(data.get("id"))
+        
+        #close crawler
+        crawler.close()
+
+
+    def test_get_data_20(self):
+
+        #Input Test Data
+        today = datetime.date.today()
+
+        api = ZaimAPI()
+        for i in range(20):
+            api.insert_payment_simple(today,100*(i + 1),"食料品")
+
+        # init crawler
+        crawler = ZaimCrawler()
+
+        # assert 
         self.assertEqual(len(crawler.get_data(today.year,today.month,20)) , 20)
 
         #clear test data
